0bf785006488bd38d46a8119a81b01200ea61b29
Michael Krebs <vault742@gmail.com>
2015-04-27 17:50:48 -0400

    submit/edit forms, gravatar, navigation

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -4,4 +4,42 @@ class RecipesController < ApplicationController
     @recipes = Recipe.all
   end
 
+  def show
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = Recipe.new(recipe_params)
+    @recipe.chef = Chef.find(2) #temporary until user auth added
+
+    if @recipe.save
+      flash[:success] = "Your recipe was created!"
+      redirect_to recipes_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def update
+    @recipe = Recipe.find(params[:id])
+    if @recipe.update(recipe_params)
+      flash[:success] = "Your recipe was updated!"
+      redirect_to recipe_path(@recipe)
+    else
+      render :edit
+    end
+  end
+
+  private
+    def recipe_params
+      params.require(:recipe).permit(:name, :summary, :description)
+    end
 end
