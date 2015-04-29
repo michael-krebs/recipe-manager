27a54861699f41c4da4aa057ba43d7b8b1e3a31d
Michael Krebs <vault742@gmail.com>
2015-04-29 13:17:32 -0400

    user authentication

---
+++ app/controllers/chefs_controller.rb
@@ -0,0 +1,27 @@
+class ChefsController < ApplicationController
+
+  def new
+    @chef = Chef.new
+  end
+
+  def create
+    @chef = Chef.new(chef_params)
+    if @chef.save
+      flash[:success] = "Your profile was created!"
+      redirect_to recipes_path
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  end 
+
+  private
+    def chef_params
+      params.require(:chef).permit(:chefname, :email, :password)
+    end
+end
