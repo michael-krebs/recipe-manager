05f76ee7385534699d7514e1545c85fd269eac0a
Michael Krebs <vault742@gmail.com>
2015-04-30 17:41:15 -0400

    authentication and site navigation

--- app/controllers/chefs_controller.rb
+++ app/controllers/chefs_controller.rb
@@ -1,4 +1,6 @@
 class ChefsController < ApplicationController
+  before_action :set_chef, only: [:edit, :update, :show]
+  before_action :require_same_user, only: [:edit, :update]
 
   def index
     @chefs = Chef.paginate(page: params[:page], per_page: 4)
@@ -20,11 +22,9 @@ class ChefsController < ApplicationController
   end
 
   def edit
-    @chef = Chef.find(params[:id])
   end
 
   def update
-    @chef = Chef.find(params[:id])
     if @chef.update(chef_params)
       flash[:success] = "Your profile was updated!"
       redirect_to recipes_path
@@ -34,12 +34,22 @@ class ChefsController < ApplicationController
   end 
 
   def show
-    @chef = Chef.find(params[:id])
     @recipes = @chef.recipes.paginate(page: params[:page], per_page: 4)
   end
 
   private
     def chef_params
       params.require(:chef).permit(:chefname, :email, :password)
+    end
+
+    def set_chef
+      @chef = Chef.find(params[:id])
+    end
+
+    def require_same_user
+      if current_user != @chef
+        flash[:danger] = "You can only edit your own profile."
+        redirect_to root_path
+      end
     end
 end
