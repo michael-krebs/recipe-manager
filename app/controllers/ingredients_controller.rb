b427eae84773f4f05e62a1696493154481225170
Michael Krebs <vault742@gmail.com>
2015-05-01 20:07:33 -0400

    ingredient and style controllers and views

---
+++ app/controllers/ingredients_controller.rb
@@ -0,0 +1,21 @@
+class IngredientsController < ApplicationController
+
+  def new
+    @ingredient = Ingredient.new
+  end
+
+  def create
+    @ingredient = Ingredient.new(ingredient_params)
+    if @ingredient.save
+      flash[:success] = "Your ingredient has been created!"
+      redirect_to recipes_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+    def ingredient_params
+      params.require(:ingredient).permit(:name)
+    end
+end
